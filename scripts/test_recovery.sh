9e6af45c682e30767704e3a7b8d5b4230a214a11
Hadar Cohen <hacohen@redhat.com>
2025-05-22 12:16:25 +0300

    fix the script

--- scripts/test_recovery.sh
+++ scripts/test_recovery.sh
@@ -2,15 +2,16 @@
 
 echo "Test Starting..."
 
-URL="https://story-app-rh-ee-hacohen-dev.apps.rm2.thpm.p1.openshiftapps.com/"
+URL="https://story-app-rh-ee-hacohen-dev.apps.rm2.thpm.p1.openshiftapps.com"
 
 for i in {1..101}
 do
     echo "Sending request $i/101"
     curl -s -X POST $URL \
-    -H "Content-Type: application/x-www-form-urlencoded" \
-    -d "name=Alex&hobbies=climbing,reading" \
-    -w "\nStatus: %{http_code}\n"
+        -H "Content-Type: application/x-www-form-urlencoded" \
+        -d "name=Alexy&hobbies=climbing,reading" \
+        -w "\nStatus: %{http_code}\n"
+    sleep 0.2
 done
 
 echo "See that the app is recovering" 
